650554c8a1f2dd3ef93a98845392ab37daeb43f5
lvicanata7 <108139004+lvicanata7@users.noreply.github.com>
2022-11-19 09:37:07 +0200

    Update main.js

--- Salon/main.js
+++ Salon/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel();
 });
+
+
+$('#owl-car-mobile').owlCarousel({
+    loop:true,
+    margin:10,
+    items:1,
+    nav: true
+})
+
+
+$('#owl-car-pc').owlCarousel({
+    loop:true,
+    items:6,
+    nav: true
+})
